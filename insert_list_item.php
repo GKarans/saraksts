ae91a2453393f55639c9fc02feb30aea01a848b1
Filips <filips.saberts@gmail.com>
2023-10-07 16:41:54 +0300

    Saraksta izveide, ievade, modificēšana, AJAX

---
+++ insert_list_item.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+include_once 'connection.php';
+
+$message = [];
+
+// Atrodam sarakstu, kuram pieder ieraksts, lai salīdzinātu lietotājvārdus
+$query = $datubaze->prepare('
+SELECT *
+FROM saraksts
+WHERE id = ?
+');
+$query->bind_param('i',$_POST['saraksts_id']);
+$query->execute();
+$result = $query->get_result();
+$saraksts = $result->fetch_object();
+
+// HTTP-request var tikt izsaukts no jebkura URL, tāpēc ir svarīgi pārbaudīt, vai lietotājs ir autorizējies un vai viņš ir saraksta īpašnieks
+if( empty($_POST['saraksts_id']) || $result->num_rows == 0){
+    $message['response'] = '404'; // not found
+}elseif( $_SESSION['username'] != $saraksts->lietotajvards){
+    $message['response'] = '403'; // forbidden
+}else{
+    // ievietojam jaunu rindu tabulā `ieraksts`
+    $query = $datubaze->prepare('
+        INSERT INTO ieraksts(teksts,saraksts_id)
+        VALUES (?,?)
+    ');
+    $query->bind_param('si', $_POST['teksts'], $_POST['saraksts_id']);
+    $query->execute();
+    $message['response'] = '200'; // OK
+    $message['id'] = $query->insert_id;
+}
+
+// Atgriežam vērtību kā JSON tekstu, lai to varētu nolasīt ar JavaScript
+echo json_encode($message);
--- list.php
+++ list.php
@@ -13,12 +13,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php include "modules/nav.php"; ?>
 
     <?php 
+        // Atrodam sarakstu, kuru lietotājs vēlas apskatīt
         $query = $datubaze->prepare('
             SELECT *
             FROM saraksts
@@ -29,12 +31,14 @@
         $result = $query->get_result();
         $saraksts = $result->fetch_object();
 
+        // Atrodam saraksta ierakstus
         $query2 = $datubaze->prepare('
             SELECT *
             FROM ieraksts
             WHERE saraksts_id = ?
         ');
 
+        // ja saraksts neeksistē vai lietotājs nav saraksta īpašnieks, tad izvadam atbilstošu ziņojumu
         if( empty($_GET['id']) || $result->num_rows == 0):
             include "modules/not_found.php";
         elseif( $_SESSION['username'] != $saraksts->lietotajvards):
@@ -61,13 +65,85 @@
                             while($ieraksts = $ieraksti->fetch_object()){
                                 $klase = "text-decoration-line-through";
                                 $klase = ($ieraksts->izsvitrots == 1) ? $klase : '';
-                                echo "<li class=\"list-group-item $klase\">" . htmlspecialchars($ieraksts->teksts) . "</li>";
+echo "<li class=\"list-group-item $klase\" data-id=\"$ieraksts->id\">" . htmlspecialchars($ieraksts->teksts) . "</li>";
                             }
                         ?>
                     </ul>
                 </div>
             </div>
         </div>
+        <script>
+            // saglabājam aktuālā saraksta ID kā javascript mainīgo. 
+            // Tas palīdzēs ievietot jaunus ierakstus un noteikt saraksta autoru
+            const saraksts_id = <?php echo $_GET['id']; ?>; 
+
+            let teksts = document.getElementById("task");
+            let poga = document.getElementById("addTask");
+            let saraksts = document.getElementById("taskList");
+
+            function ievietotSarakstā(){
+                if(teksts.value.trim() != ""){
+                    /** 
+                     * AJAX – Asynchronous JavaScript And XML.
+                     * https://www.w3schools.com/xml/ajax_intro.asp
+                     * .ajax() izveido HTTP-request pieprasījumu, un gaida atbildi no servera 
+                     */ 
+                    $.ajax({ 
+                        type:'POST',
+                        url: 'insert_list_item.php',
+                        data: {
+                            teksts: teksts.value,
+                            saraksts_id: saraksts_id,
+                        },
+                        dataType: 'json',
+                        encode: true,
+                    }).done(function (data) { // reaģējam uz servera atbildi
+                        console.log(data);
+
+                        if(data.response == '200'){
+                            // konstruējam <li> elementu
+                            let li = document.createElement("li");
+                            li.classList.add("list-group-item");
+                            li.innerText = teksts.value;
+                            li.setAttribute('data-id', data.id);
+
+                            saraksts.appendChild(li);
+                        }
+                    });
+                }
+            }
+
+            poga.addEventListener("click", ievietotSarakstā );
+            teksts.addEventListener("keypress", function(e){
+                if(e.key == "Enter"){
+                    ievietotSarakstā();
+                }
+            });
+            
+            // "Event delegation"
+            // ne visi saraksta elementi ir pieejami, kad mēs ielādējam lapu, līdz ar to nepietiek ar to, ka uzstādam event listener dokumenta ielādēšanās brīdī
+            // Izmantojot jQuery, deliģējam document objektu klausīties klikšķi uz kādu saraksta elementu
+            // https://learn.jquery.com/events/event-delegation/
+            $(document).on('click', '#taskList li', function(){
+
+                $(this).toggleClass("text-decoration-line-through");
+
+                $.ajax({
+                    type:'POST',
+                    url: 'toggle_list_item.php',
+                    data: {
+                        ieraksts_id: $(this).attr('data-id'),
+                        saraksts_id: saraksts_id,
+                    },
+                    dataType: 'json',
+                    encode: true,
+                }).done(function (data) {
+                    console.log(data);
+                });
+
+            });
+
+        </script>
     <?php endif; ?>
 
 </body>
